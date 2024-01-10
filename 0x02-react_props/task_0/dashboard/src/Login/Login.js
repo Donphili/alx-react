a61fec9db7e2c97ea82cb8b2651050b477ac5c5b
paulkelvin <ibunadesanya0@gmail.com>
2024-01-10 17:34:58 +0000

    0x01-react_intro

--- 0x02-react_props/task_0/dashboard/src/Login/Login.js
+++ 0x02-react_props/task_0/dashboard/src/Login/Login.js
@@ -5,10 +5,10 @@ function Login() {
   return (
     <React.Fragment>
       <div className="App-body">
-        <p>Login to access the full dashboard</p>
+        <p>Login to Access the full dashboard</p>
         <form>
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email"></input>
+          <input type="email"name="email"></input>
           <label htmlFor="password">Password:</label>
           <input type="password" name="password"></input>
           <button>OK</button>
--- 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Notifications from './Notifications';
+import React from "react";
+import { shallow } from "enzyme";
+import Notifications from "./Notifications";
 
-describe('Notifications Component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.exists()).toBe(true);
+describe("Notification component tests", () => {
+  it("renders Notification component without crashing", () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification).toBeDefined();
   });
 
-  it('renders three list items', () => {
-    const wrapper = shallow(<Notifications />);
-    const listItems = wrapper.find('ul li');
-    expect(listItems).toHaveLength(3);
+  it("renders ul", () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification.find("ul")).toBeDefined();
   });
 
-  it('renders the text Here is the list of notifications', () => {
-    const wrapper = shallow(<Notifications />);
-    const text = 'Here is the list of notifications';
-    expect(wrapper.contains(text)).toBe(true);
+  it("renders three list items", () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification.find("li")).toHaveLength(3);
+  });
+
+  it("renders correct text", () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification.find("p").text()).toBe("Here is the list of notifications");
   });
 });
